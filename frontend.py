57b159ef25ed2043a7a8d7ee386238f5123a668b
Sameer Gupta <sameerguptaa09@gmail.com>
2025-09-04 02:12:20 +0530

    integrated sqlite

--- frontend.py
+++ frontend.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from backend import chatbot
-from langchain_core.messages import HumanMessage, AIMessage
+from backend import chatbot, retrieve_all_threads
+from langchain_core.messages import HumanMessage
 import uuid
 
 # **************************************** utility functions *************************
@@ -33,7 +33,7 @@ if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id()
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = []
+    st.session_state['chat_threads'] = retrieve_all_threads()
 
 add_thread(st.session_state['thread_id'])
 
@@ -80,20 +80,25 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+    #CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
 
-     # first add the message to message_history
-    with st.chat_message("assistant"):
-        def ai_only_stream():
-            for message_chunk, metadata in chatbot.stream(
-                {"messages": [HumanMessage(content=user_input)]},
-                config=CONFIG,
-                stream_mode="messages"
-            ):
-                if isinstance(message_chunk, AIMessage):
-                    # yield only assistant tokens
-                    yield message_chunk.content
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {
+            "thread_id": st.session_state["thread_id"]
+        },
+        "run_name": "chat_turn",
+    }
 
-        ai_message = st.write_stream(ai_only_stream())
+    # first add the message to message_history
+    with st.chat_message('assistant'):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= CONFIG,
+                stream_mode= 'messages'
+            )
+        )
 
     st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
