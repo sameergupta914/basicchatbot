6068a20bfcd3af8db60c3692fd7971025f5d6939
Sameer Gupta <sameerguptaa09@gmail.com>
2025-09-04 00:52:16 +0530

    added streaming

--- frontend.py
+++ frontend.py
@@ -25,10 +25,23 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    # response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
     
-    ai_message = response['messages'][-1].content
-    # first add the message to message_history
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    # ai_message = response['messages'][-1].content
+    # # first add the message to message_history
+    # st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    # with st.chat_message('assistant'):
+    #     st.text(ai_message)
+    
     with st.chat_message('assistant'):
-        st.text(ai_message)
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode= 'messages'
+            )
+        )
+        delay=0.5
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
